04d404732646a1e8ed3c11815f0cb74b9194456e
Jaimin <jaimin.d.88@gmail.com>
2018-02-03 14:48:43 -0500

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-import Canvas from './canvas';
+import Canvas from './components/canvas';
 
 class App extends Component {
 	render() {
 		let height = window.innerHeight;
 		let width = window.innerWidth * 0.8;
-		
+
 		return (
 			<div className="App">
 				<Canvas width={width} height={height}/>
