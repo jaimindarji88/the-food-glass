81fad00fe93a9dfb5f036a3d54ffe0cec6d9b7c3
Jaimin <jaimin.d.88@gmail.com>
2018-02-03 17:33:51 -0500

    added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Canvas from './components/canvas';
 class App extends Component {
 	render() {
 		let height = window.innerHeight;
-		let width = window.innerWidth * 0.8;
+		let width = window.innerWidth * 0.7;
 
 		return (
 			<div className="App">
-				<Canvas width={width} height={height}/>
+				<Canvas width={width} height={height} />
 			</div>
 		);
 	}
